7b27ec289289b1bcf5e09f3745abde46d2a6dd03
lakshman1803 <mucharlalakshman@gmail.com>
2023-04-06 04:53:05 +0000

    large

--- ex-sample.c
+++ ex-sample.c
@@ -1,7 +1,14 @@
-class main()
+class main add()
+
 {
 	public static void main(strgs )
 		s.o.p.ln(add)
 }
+<<<<<<< Updated upstream
 hey  hai
+=======
+hey hai
+>>>>>>> Stashed changes
 
+hello everyone
+good mrng
